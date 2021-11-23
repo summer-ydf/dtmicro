31d78d16dbfeb9e3f9c9575a0fd7ea81cbbbc5b7
yandongfa <1973984292@qq.com>
2021-11-23 15:25:25 +0800

    添加nacos服务注册

--- dt-cloud-service/dt-service-workflow/src/main/java/com/cms/workflow/WorkFlowApplication.java
+++ dt-cloud-service/dt-service-workflow/src/main/java/com/cms/workflow/WorkFlowApplication.java
@@ -1,18 +1,18 @@
-package com.dt.manage;
+package com.cms.workflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
- * 核心模块主启动
- * @author ydf Created by 2021/11/23 14:54
+ * 流程模块主启动类
+ * @author ydf Created by 2021/11/23 15:23
  */
 @EnableDiscoveryClient
 @SpringBootApplication
-public class ManageApplication {
+public class WorkFlowApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ManageApplication.class,args);
+        SpringApplication.run(WorkFlowApplication.class,args);
     }
 }
