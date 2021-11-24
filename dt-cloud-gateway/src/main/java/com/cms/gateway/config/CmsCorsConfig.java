3a784914bc0be61cdb200d6f4bdf07de1ff1aa41
1973984292 <1973984292@qq.com>
2021-11-24 19:05:02 +0800

    '添加前后端分离跨越配置'

---
+++ dt-cloud-gateway/src/main/java/com/cms/gateway/config/CmsCorsConfig.java
@@ -0,0 +1,32 @@
+package com.cms.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+/**
+ * @author ydf Created by 2021/11/24 17:57
+ */
+@Configuration
+public class CmsCorsConfig {
+
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        org.springframework.web.cors.UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig());
+        return new CorsFilter(source);
+    }
+}
+
