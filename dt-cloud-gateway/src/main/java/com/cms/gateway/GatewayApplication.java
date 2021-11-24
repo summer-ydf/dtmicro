c63d563b1ffa890d5269344462b40aec1dbc693b
yandongfa <1973984292@qq.com>
2021-11-24 17:40:25 +0800

    添加gatway网关鉴权

---
+++ dt-cloud-gateway/src/main/java/com/cms/gateway/GatewayApplication.java
@@ -0,0 +1,15 @@
+package com.cms.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ydf Created by 2021/11/24 17:05
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class,args);
+    }
+}
