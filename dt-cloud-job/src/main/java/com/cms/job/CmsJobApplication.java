88d35d06c935ac7ede680c0625405b7c9fae85e2
yandongfa <1973984292@qq.com>
2021-11-25 11:50:21 +0800

    添加定时调度模块

---
+++ dt-cloud-job/src/main/java/com/cms/job/CmsJobApplication.java
@@ -0,0 +1,15 @@
+package com.cms.job;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ydf Created by 2021/11/25 11:49
+ */
+@SpringBootApplication
+public class CmsJobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CmsJobApplication.class,args);
+    }
+}
