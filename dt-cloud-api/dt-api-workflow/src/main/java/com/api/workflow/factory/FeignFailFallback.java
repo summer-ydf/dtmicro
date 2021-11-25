0f33a9e11674ebdd089f263508f2526fb8decfe1
yandongfa <1973984292@qq.com>
2021-11-25 11:55:09 +0800

    集成common模块

--- dt-cloud-api/dt-api-workflow/src/main/java/com/api/workflow/factory/FeignFailFallback.java
+++ dt-cloud-api/dt-api-workflow/src/main/java/com/api/workflow/factory/FeignFailFallback.java
@@ -1,5 +1,6 @@
 package com.api.workflow.factory;
 
+import com.cms.common.result.ResultUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,12 +11,8 @@ public interface FeignFailFallback {
 
     Logger log = LoggerFactory.getLogger(FeignFailFallback.class);
 
-//    default <T> R<T> fail(){
-//        return R.fail("业务模块未加载");
-//    }
-
-    default String fail() {
+    default <T> ResultUtil<T> fail() {
         log.info("调用失败统一处理返回：服务未开启====================");
-        return "业务模块未加载";
+        return ResultUtil.error("业务模块未启动");
     }
 }
