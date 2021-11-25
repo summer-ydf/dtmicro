7871052ba24c3a127f686d5cb503adb238589799
yandongfa <1973984292@qq.com>
2021-11-25 11:43:43 +0800

    添加公共模块

--- dt-cloud-common/src/main/java/com/cms/common/utils/SysUuidUtils.java
+++ dt-cloud-common/src/main/java/com/cms/common/utils/SysUuidUtils.java
@@ -1,53 +1,48 @@
 package com.cms.common.utils;
 
-import java.util.UUID;
+import cn.hutool.core.lang.Snowflake;
+import cn.hutool.core.util.IdUtil;
 
 /**
+ * 全局UUID工具类
  * @author ydf Created by 2021/11/25 11:22
  */
 public class SysUuidUtils {
 
-    private static final int SHORT_LENGTH = 8;
-
-    public static String[] chars = new String[] { "a", "b", "c", "d", "e", "f",
-            "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
-            "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "jqGrid-4.4.3", "4", "5",
-            "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I",
-            "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
-            "W", "X", "Y", "Z" };
-
-    public static String uuid() {
-        String str = UUID.randomUUID().toString();
-        return str.replace("-","");
+    /**
+     * 生成的UUID是带-的字符串
+     * @return 返回UUID
+     */
+    public static String randomUUID() {
+        return IdUtil.randomUUID();
     }
 
-    public static String generateShortUuid() {
-        StringBuffer shortBuffer = new StringBuffer();
-        String uuid = UUID.randomUUID().toString().replace("-", "");
-        for (int i = 0; i < SHORT_LENGTH; i++) {
-            String str = uuid.substring(i * 4, i * 4 + 4);
-            int x = Integer.parseInt(str, 16);
-            shortBuffer.append(chars[x % 0x3E]);
-        }
-        return shortBuffer.toString();
+    /**
+     * 生成的UUID是不带-的字符串
+     * @return 返回UUID
+     */
+    public static String simpleUUID() {
+        return IdUtil.simpleUUID();
     }
 
-    public static String getUniqueIdByUUId() {
-        //最大支持1-9个集群机器部署
-        int machineId = 1;
-        int hashCodeV = UUID.randomUUID().toString().hashCode();
-        if(hashCodeV < 0) {
-            hashCodeV = - hashCodeV;
-        }
-        // 0 代表前面补充0
-        // 4 代表长度为4
-        // d 代表参数为正数型
-        return machineId + String.format("%015d", hashCodeV);
+    /**
+     * 生成类似：5b9e306a4df4f8c54a39fb0c
+     * @return 返回UUID
+     */
+    public static String objectUUID() {
+        return IdUtil.objectId();
+    }
+
+    /**
+     * 分布式唯一ID
+     * @return 返回ID
+     */
+    public static Long nextId() {
+        Snowflake snowflake =IdUtil.createSnowflake(1,1);
+        return snowflake.nextId();
     }
 
     public static void main(String[] args) {
-        System.out.println(getUniqueIdByUUId());
-        System.out.println(uuid());
-        System.out.println(generateShortUuid());
+        System.out.println(nextId());
     }
 }
