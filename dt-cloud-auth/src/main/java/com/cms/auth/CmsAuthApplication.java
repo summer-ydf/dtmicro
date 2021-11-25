5780f5e75f6cd1370398b290848c60baace2fd7a
yandongfa <1973984292@qq.com>
2021-11-25 11:48:00 +0800

    添加鉴权模块

---
+++ dt-cloud-auth/src/main/java/com/cms/auth/CmsAuthApplication.java
@@ -0,0 +1,15 @@
+package com.cms.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ydf Created by 2021/11/25 11:45
+ */
+@SpringBootApplication
+public class CmsAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CmsAuthApplication.class,args);
+    }
+}
