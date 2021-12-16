4085596dc32e0431651839df5a9312252af09349
yandongfa <1973984292@qq.com>
2021-12-16 15:10:33 +0800

    添加

--- dt-cloud-service/dt-service-manage/src/main/java/com/cms/manage/CmsManageApplication.java
+++ dt-cloud-service/dt-service-manage/src/main/java/com/cms/manage/CmsManageApplication.java
@@ -1,16 +1,20 @@
 package com.cms.manage;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * 核心模块主启动
  * @author ydf Created by 2021/11/23 14:54
  */
 @SpringBootApplication
+@EnableTransactionManagement
 @EnableDiscoveryClient
+@MapperScan(basePackages = {"com.cms.workflow.mapper"})
 @EnableFeignClients(basePackages ={"com.api.*.feign"})
 public class CmsManageApplication {
 
