dee283cb6a5261c0f6af4d87b4bb9b8f3aff5753
1973984292 <1973984292@qq.com>
2022-01-06 21:41:17 +0800

    '修改授权服务'

--- dt-cloud-auth/src/main/java/com/cms/auth/config/AccessTokenConfig.java
+++ dt-cloud-auth/src/main/java/com/cms/auth/config/AccessTokenConfig.java
@@ -15,24 +15,9 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @Configuration
 public class AccessTokenConfig {
 
-    /**
-     * 密钥
-     */
-    public static final String SIGNING_KET = "uaa123";
-
     @Bean
     public TokenStore tokenStore() {
-        // 采用内存的方式
+        // TODO 采用内存的方式
         return new InMemoryTokenStore();
-        // JWT令牌存储方案
-        //return new JwtTokenStore(accessTokenConverter());
-    }
-
-    @Bean
-    public JwtAccessTokenConverter accessTokenConverter() {
-        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-        // 对称密钥，资源服务器使用该密钥来验证
-        converter.setSigningKey(SIGNING_KET);
-        return converter;
     }
 }
