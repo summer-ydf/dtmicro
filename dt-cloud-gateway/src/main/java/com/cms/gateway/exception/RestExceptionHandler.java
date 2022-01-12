5c00b1114c0dc46b750a716263d162ecec6f5a47
yandongfa <1973984292@qq.com>
2022-01-12 15:01:29 +0800

    修改授权服务

--- dt-cloud-gateway/src/main/java/com/cms/gateway/exception/RestExceptionHandler.java
+++ dt-cloud-gateway/src/main/java/com/cms/gateway/exception/RestExceptionHandler.java
@@ -1,28 +1,25 @@
 package com.cms.gateway.exception;
 
+import com.cms.common.utils.SysCmsUtils;
 import com.cms.gateway.GatewayConstant;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler;
 import org.springframework.cloud.gateway.support.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-import javax.validation.constraints.NotNull;
-
 /**
  * 网关异常通用处理器，只作用在webflux 环境下
  */
-@Slf4j
 public class RestExceptionHandler implements ErrorWebExceptionHandler {
 
     @Override
     public Mono<Void> handle(ServerWebExchange exchange, Throwable ex) {
         if(ex instanceof NotFoundException){
-            log.warn("not found ex->{}",ex.getMessage());
+            SysCmsUtils.log.warn("not found ex->{}",ex.getMessage());
             return GatewayConstant.response(exchange, HttpStatus.SERVICE_UNAVAILABLE, GatewayConstant.UNAVAILABLE_TEXT, GatewayConstant.UNAVAILABLE_JSON);
         }else {
-            log.warn("捕捉到异常:",ex);
+            SysCmsUtils.log.warn("捕捉到异常:",ex);
         }
         return Mono.empty();
     }
