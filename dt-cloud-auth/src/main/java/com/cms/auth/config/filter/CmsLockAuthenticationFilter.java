59ae78c66a10ff79a41a6c9002484bf1f0df6a21
yandongfa <1973984292@qq.com>
2022-01-22 10:36:12 +0800

    添加账号锁定校验

--- dt-cloud-auth/src/main/java/com/cms/auth/config/filter/CmsLockAuthenticationFilter.java
+++ dt-cloud-auth/src/main/java/com/cms/auth/config/filter/CmsLockAuthenticationFilter.java
@@ -3,16 +3,15 @@ package com.cms.auth.config.filter;
 import com.cms.auth.exception.ParameterAuthenticationException;
 import com.cms.common.constant.ConstantCommonCode;
 import org.apache.commons.lang.StringUtils;
-import org.apache.commons.lang.math.NumberUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import javax.servlet.FilterChain;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Date;
 
 /**
+ * 账号锁定校验
  * @author ydf Created by 2022/1/21 17:55
  */
 public class CmsLockAuthenticationFilter extends CmsAbstractBasicAuthenticationFilter {
@@ -21,18 +20,15 @@ public class CmsLockAuthenticationFilter extends CmsAbstractBasicAuthenticationF
     private StringRedisTemplate stringRedisTemplate;
 
     @Override
-    protected void handle(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws Exception {
-        String username=request.getParameter("username");
-        String password=request.getParameter("password");
-        if(StringUtils.isEmpty(username)) {throw new ParameterAuthenticationException("缺少参数"); }
-        if(StringUtils.isEmpty(password)) {throw new ParameterAuthenticationException("缺少参数"); }
-        username = username.trim();
-        String login_count_str = stringRedisTemplate.opsForValue().get(ConstantCommonCode.CACHE_LOGIN_LOCK + username);
-        long login_count = NumberUtils.toLong(login_count_str,0);
-        if(login_count == 5) {
-            Date exp_date = new Date(login_count);
-            if(new Date().before(exp_date)) {
-                throw new ParameterAuthenticationException("账户被锁定，请稍后登录");
+    protected void handle(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        if(StringUtils.isEmpty(username)) {throw new ParameterAuthenticationException("缺少username参数"); }
+        if(StringUtils.isEmpty(password)) {throw new ParameterAuthenticationException("缺少password参数"); }
+        String loginCountStr = stringRedisTemplate.opsForValue().get(ConstantCommonCode.CACHE_LOGIN_LOCK + username.trim());
+        if(loginCountStr != null && loginCountStr.length() > 1) {
+            if(System.currentTimeMillis() < Long.parseLong(loginCountStr)) {
+                throw new ParameterAuthenticationException("账户被锁定15分钟，请稍后登录!");
             }
         }
     }
