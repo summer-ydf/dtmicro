a21c7c3611d98ea9f0a4ecfb22a4bba531ba114a
1973984292 <1973984292@qq.com>
2022-02-21 22:16:07 +0800

    '添加获取菜单接口'

--- dt-cloud-service/dt-service-manage/src/main/java/com/cms/manage/controller/SysMenuController.java
+++ dt-cloud-service/dt-service-manage/src/main/java/com/cms/manage/controller/SysMenuController.java
@@ -5,7 +5,6 @@ import com.cms.manage.entity.SysMenuEntity;
 import com.cms.manage.service.SysMenuService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
