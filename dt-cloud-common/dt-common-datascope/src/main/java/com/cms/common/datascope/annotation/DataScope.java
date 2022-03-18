1318dce181579238d06c49ce032367fc65f1b527
yandongfa <1973984292@qq.com>
2022-03-18 17:55:10 +0800

    添加角色数据权限控制

---
+++ dt-cloud-common/dt-common-datascope/src/main/java/com/cms/common/datascope/annotation/DataScope.java
@@ -0,0 +1,27 @@
+package com.cms.common.datascope.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 角色数据权限过滤注解
+ * @author ydf Created by 2022/3/18 17:27
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface DataScope {
+
+    /**
+     * 部门表的别名
+     */
+    String deptAlias() default "";
+
+    /**
+     * 用户表的别名
+     */
+    String userAlias() default "";
+}
