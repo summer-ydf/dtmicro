597322fefb8f3136c9ad5db3b896c732c012d9c9
yandongfa <1973984292@qq.com>
2022-04-15 17:57:25 +0800

    添加系统配置类

--- dt-cloud-service/dt-service-manage/src/main/java/com/cms/manage/controller/SysSettingController.java
+++ dt-cloud-service/dt-service-manage/src/main/java/com/cms/manage/controller/SysSettingController.java
@@ -32,7 +32,6 @@ public class SysSettingController {
     @ApiOperation(value = "获取我的常用应用")
     @GetMapping("/get_my_setting/{userId}")
     public ResultUtil<SysOperatorSettingEntity> getMySetting(@PathVariable Long userId) {
-
         SysOperatorSettingEntity settingEntity = this.sysSettingService.getOne(new QueryWrapper<SysOperatorSettingEntity>().eq("user_id", userId));
         return ResultUtil.success(settingEntity);
     }
---
+++ dt-cloud-service/dt-service-manage/src/main/java/com/cms/manage/entity/SysConfigEntity.java
@@ -0,0 +1,40 @@
+package com.cms.manage.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.cms.common.jdbc.domain.BaseEntity;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * @author ydf Created by 2022/4/15 17:54
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@TableName("sys_config")
+@ApiModel(value="系统配置对象")
+@EqualsAndHashCode(callSuper = true)
+public class SysConfigEntity extends BaseEntity implements Serializable {
+
+    @ApiModelProperty(value = "主键")
+    @TableId(type = IdType.INPUT)
+    private String id;
+
+    @ApiModelProperty(value = "配置key")
+    private String k;
+
+    @ApiModelProperty(value = "配置value")
+    private String v;
+
+    @ApiModelProperty(value = "配置类型")
+    private String type;
+
+}
