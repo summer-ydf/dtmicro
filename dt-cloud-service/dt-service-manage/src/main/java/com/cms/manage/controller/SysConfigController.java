404f1a826e853d9750b959a40da6e7739f4b3b00
yandongfa <1973984292@qq.com>
2022-04-20 17:08:59 +0800

    添加缓存

--- dt-cloud-service/dt-service-manage/src/main/java/com/cms/manage/controller/SysConfigController.java
+++ dt-cloud-service/dt-service-manage/src/main/java/com/cms/manage/controller/SysConfigController.java
@@ -2,9 +2,7 @@ package com.cms.manage.controller;
 
 import com.cms.common.core.builder.SettingBuilders;
 import com.cms.common.core.domain.Params;
-import com.cms.common.core.domain.SysConfig;
 import com.cms.common.tool.result.ResultUtil;
-import com.cms.manage.entity.SysConfigEntity;
 import com.cms.manage.service.SysConfigService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -14,9 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Set;
 
 /**
@@ -46,13 +42,8 @@ public class SysConfigController {
         Set<String> keySet = body.keySet();
         Params params = new Params();
         for (String k : keySet) {
-            System.out.println(k);
-            System.out.println(body.get(k));
             params.put(k,body.get(k));
         }
-        System.out.println(params);
-        List<SysConfig> sysConfigs = SettingBuilders.settingParamsToList(params);
-        System.out.println("添加的数据->>>"+sysConfigs);
-        return sysConfigService.saveConfigs(sysConfigs);
+        return sysConfigService.saveConfigs(SettingBuilders.settingParamsToList(params));
     }
 }
