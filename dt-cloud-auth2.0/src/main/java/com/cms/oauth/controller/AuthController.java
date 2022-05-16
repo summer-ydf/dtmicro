42102dd609502017f550d7d6ef5b79bb86bd6750
1973984292 <1973984292@qq.com>
2022-05-16 23:06:54 +0800

    '添加授权码模式'

--- dt-cloud-auth2.0/src/main/java/com/cms/oauth/controller/AuthController.java
+++ dt-cloud-auth2.0/src/main/java/com/cms/oauth/controller/AuthController.java
@@ -13,6 +13,7 @@ public class AuthController {
     @RequestMapping(value = "/myLogin")
     public String login() {
         // https://www.cnblogs.com/lzj123/p/10279007.html
+        // https://gitee.com/UnityAlvin/gulimall/commit/222302f1e34f78d8f0d5045363857fb693604608
         System.out.println("返回自定义登录页面");
         return "login";
     }
