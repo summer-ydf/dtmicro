06a11eb469c1792764a49d8a19e1cfd0c8426ba8
1973984292 <1973984292@qq.com>
2022-05-28 10:59:47 +0800

    '优化代码片段'

--- dt-cloud-service/dt-service-manage/src/main/java/com/cms/manage/consumer/JobSettingCallBacker.java
+++ dt-cloud-service/dt-service-manage/src/main/java/com/cms/manage/consumer/JobSettingCallBacker.java
@@ -1,16 +1,18 @@
 package com.cms.manage.consumer;
 
 import com.cms.common.core.builder.SettingModelDataCallBacker;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * 任务监听处理类
  * @author DT辰白 Created by 2022/4/19 15:50
  */
+@Slf4j
 public class JobSettingCallBacker implements SettingModelDataCallBacker {
 
     @Override
     public void callback(Object oldValue, Object newValue) {
-        System.out.println("新的值："+newValue);
-        System.out.println("旧的值："+oldValue);
+        log.info("新的值【{}】",newValue);
+        log.info("旧的值【{}】",oldValue);
     }
 }
